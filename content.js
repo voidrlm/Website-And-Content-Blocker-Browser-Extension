f730c5a38cd41e1484961755582354612a56d3a7
VOIDRLM <varjun609@gmail.com>
2023-04-16 14:45:22 +0530

    Added implementation to block by content

--- content.js
+++ content.js
@@ -7,8 +7,25 @@ document.body.addEventListener('click', block);
 document.body.addEventListener('keyup', block);
 function block() {
     let currentDomainBlocked = JSON.parse(localStorage.getItem('blockThisSite')) || false;
+    var blockByFilter = false;
+    if (!currentDomainBlocked) {
+        const url = window.location.href;
+        const title = document.title;
+        //ADD YOUR  FILTERS BELOW
+        //Example : www.facebook.com or facebook
+        const filters = [];
 
-    if (currentDomainBlocked) {
+        if (filters.length !== 0) {
+            for (let i = 0; i < filters.length; i++) {
+                if (url.includes(filters[i]) || title.includes(filters[i])) {
+                    blockByFilter = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (currentDomainBlocked || blockByFilter) {
         let elements = document.querySelectorAll('html');
         for (let t = 0; t < elements.length; t++) {
             elements[t].style.display = 'none';
