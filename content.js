b7dd829f1411a2ed4ad1f155a0cc5cd0e12e6e7f
VOIDRLM <varjun609@gmail.com>
2023-04-16 14:43:15 +0530

    Setting local storage data for domain identification and unblocking

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
+block();
 chrome.runtime.onMessage.addListener(function () {
     activateBlock();
 });
 
-function activateBlock() {}
+function block() {}
+function activateBlock() {
+    if (JSON.parse(localStorage.getItem('blockThisSite'))) {
+        localStorage.setItem('blockThisSite', false);
+        window.location.reload();
+    } else {
+        localStorage.setItem('blockThisSite', true);
+        block();
+    }
+}
