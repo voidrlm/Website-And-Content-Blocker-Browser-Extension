4d25100d0de08247b85cd604e342a836bfccc7b9
VOIDRLM <varjun609@gmail.com>
2023-04-16 14:44:45 +0530

    Implementation to block domains

--- content.js
+++ content.js
@@ -5,7 +5,18 @@ chrome.runtime.onMessage.addListener(function () {
 
 document.body.addEventListener('click', block);
 document.body.addEventListener('keyup', block);
-function block() {}
+function block() {
+    let currentDomainBlocked = JSON.parse(localStorage.getItem('blockThisSite')) || false;
+
+    if (currentDomainBlocked) {
+        let elements = document.querySelectorAll('html');
+        for (let t = 0; t < elements.length; t++) {
+            elements[t].style.display = 'none';
+        }
+        let text = currentDomainBlocked ? 'DOMAIN' : 'CONTENT';
+        document.title = text + ' BLOCKED';
+    }
+}
 function activateBlock() {
     if (JSON.parse(localStorage.getItem('blockThisSite'))) {
         localStorage.setItem('blockThisSite', false);
